aab3526841832f3d45997f4f8437a4c8725e6d97
彭岳昌 <31910773+pycll@users.noreply.github.com>
2017-09-22 19:11:25 +0800

    uploaded schoolwork_2

---
+++ schoolwork_2.py
@@ -0,0 +1,9 @@
+import os,time
+print("pengyuechang")
+time.sleep(0.5)
+i=os.system('cls')
+print("    pengyuechang")
+time.sleep(0.5)
+i=os.system('cls')
+print("        pengyuechang")
+time.sleep(0.5)
